70298075d1ecf6e932ca15dab79ec6542e25f5c3
synodriver <diguohuangjiajinweijun@gmail.com>
2022-07-31 13:19:00 +0000

    update dep

--- tests/test_tea.py
+++ tests/test_tea.py
@@ -1,18 +1,34 @@
 """
 Copyright (c) 2008-2021 synodriver <synodriver@gmail.com>
 """
+import sys
+sys.path.append(".")
+
 from random import randint
 from unittest import TestCase
 import time
 
 import pytea
 import ftea
+import rtea
 
 
 class TestTea(TestCase):
     def setUp(self) -> None:
         self.old = pytea.TEA(b'1234567812345678')
         self.new = ftea.TEA(b'1234567812345678')
+
+    def test_encrypt_rs(self):
+        for i in range(10000):
+            rand_data = bytes(randint(0, 255) for _ in range(randint(1, 1000)))
+            encoded = self.new.encrypt_qq(rand_data)
+            self.assertEqual(rtea.qqtea_decrypt(encoded, b'1234567812345678'), rand_data)
+
+    def test_decrypt_rs(self):
+        for i in range(10000):
+            rand_data = bytes(randint(0, 255) for _ in range(randint(1, 1000)))
+            encoded = rtea.qqtea_encrypt(rand_data, b'1234567812345678')
+            self.assertEqual(self.new.decrypt_qq(encoded), rand_data)
 
     def test_decrypt(self):
         for i in range(10000):
@@ -29,8 +45,12 @@ class TestTea(TestCase):
         start = time.time()
         for i in range(1000):
             self.new.encrypt_qq(data)
-        print(f"new spend {time.time() - start}")
+        print(f"ftea spend {time.time() - start}")
 
+        start = time.time()
+        for i in range(1000):
+            rtea.qqtea_encrypt(data, b'1234567812345678')
+        print(f"rtea spend {time.time() - start}")
 
 if __name__ == "__main__":
     import unittest
